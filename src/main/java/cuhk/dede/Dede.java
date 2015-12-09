cc247d3843a80e5c86fbb197026b83a35a90e38e
BrianOn99 <chiubrian@link.cuhk.edu.hk>
2015-12-09 03:42:17 +0000

    raw maven project

---
+++ src/main/java/cuhk/dede/Dede.java
@@ -0,0 +1,12 @@
+package cuhk.dede;
+
+/**
+ * command line interface
+ */
+public class Dede
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/cuhk/dede/AppTest.java
@@ -0,0 +1,17 @@
+package cuhk.dede;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest
+{
+    public AppTest()
+    {
+    }
+
+    @Test
+    public void testApp()
+    {
+        assertTrue("dummy, impossible to fail", true);
+    }
+}
